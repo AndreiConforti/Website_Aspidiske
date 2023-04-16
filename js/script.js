0b4dd5da0621038d824040852c27901e3d68e0f5
devconforti <andreiconforti77@gmail.com>
2023-04-16 10:01:10 -0300

    Formatter js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const header = document.querySelector("header");
 
-window.addEventListener ("scroll", function() {
-	header.classList.toggle ("sticky", window.scrollY >0);
+window.addEventListener("scroll", function () {
+	header.classList.toggle("sticky", window.scrollY > 0);
 });
 
 let menu = document.querySelector('#menu-icon');
@@ -17,12 +17,12 @@ window.onscroll = () => {
 	navbar.classList.remove('active');
 };
 
-const sr = ScrollReveal ({
+const sr = ScrollReveal({
 	distance: '25px',
 	duration: 2500,
 	reset: true
 })
 
-sr.reveal('.home-text',{delay:190, origin:'bottom'})
+sr.reveal('.home-text', { delay: 190, origin: 'bottom' })
 
-sr.reveal('.about,.services,.portfolio,.contact',{delay:200, origin:'bottom'})
+sr.reveal('.about,.services,.portfolio,.contact', { delay: 200, origin: 'bottom' })
